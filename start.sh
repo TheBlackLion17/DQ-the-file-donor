e8d6ba56f50228573678eae5407f997a7cb81782
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2023-11-05 00:33:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TheBlackLion17/SOMU
+  git clone https://github.com/TheBlackLion17/SOMU. git/SOMU
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SOMU
